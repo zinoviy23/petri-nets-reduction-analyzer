b00cbe939028f723ba8df22d5fd3c7c5eb818765
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-02-15 21:25:02 +0300

    init commit

---
+++ petri-nets-calculation-server/src/main/java/hse/se/aaizmaylov/petrinetscalculationserver/PetriNetsCalculationServerApplication.java
@@ -0,0 +1,14 @@
+package hse.se.aaizmaylov.petrinetscalculationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetriNetsCalculationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetriNetsCalculationServerApplication.class, args);
+    }
+
+}
+
---
+++ petri-nets-calculation-server/src/test/java/hse/se/aaizmaylov/petrinetscalculationserver/PetriNetsCalculationServerApplicationTests.java
@@ -0,0 +1,17 @@
+package hse.se.aaizmaylov.petrinetscalculationserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetriNetsCalculationServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
