f891629d828d9a29f07fd87a9926444095b2b289
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-04-15 16:50:24 +0300

    Simple Reducer

--- petri-nets-library/src/main/java/hse/se/aaizmaylov/petrinetslibrary/petrinets/basic/analysis/Reducer.java
+++ petri-nets-library/src/main/java/hse/se/aaizmaylov/petrinetslibrary/petrinets/basic/analysis/Reducer.java
@@ -1,14 +1,14 @@
 package hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.analysis;
 
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.Edge;
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.analysis.DeleteVertexCallback;
 import hse.se.aaizmaylov.petrinetslibrary.petrinets.analysis.Reduction;
 import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.PetriNet;
 import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.Place;
 import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.Transition;
 import lombok.NonNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Reducer {
     private PetriNet petriNet;
@@ -18,17 +18,101 @@ public class Reducer {
     private List<Place> placesToDelete = new ArrayList<>();
     private List<Transition> transitionsToDelete = new ArrayList<>();
 
+    private Set<Place> visitedPlace = new HashSet<>();
+    private Set<Transition> visitedTransition = new HashSet<>();
+
+    private Collection<? extends Reduction<Place>> reductionsOnPlaces;
+    private Collection<? extends Reduction<Transition>> reductionsOnTransitions;
+
+    private DeleteVertexCallback<Place, Transition> deletePlaceCallback = new DeleteVertexCallback<Place, Transition>() {
+        @Override
+        public void onDeleteTarget(Place place) {
+            placesToDelete.add(place);
+        }
+
+        @Override
+        public void onDeleteNeighbour(Transition transition) {
+            transitionsToDelete.add(transition);
+        }
+    };
+
+    private DeleteVertexCallback<Transition, Place> deleteTransitionCallback =
+            DeleteVertexCallback.invertedAdapter(deletePlaceCallback);
+
     public Reducer(@NonNull PetriNet petriNet) {
         this.petriNet = petriNet;
     }
 
-    public void reduce(Collection<? extends Reduction<Place>> reductionsOnPlaces,
-                       Collection<? extends Reduction<Transition>> reductionsOnTransitions) {
+    private int reduceCount = 0;
+
+    public void reduce(@NonNull Collection<? extends Reduction<Place>> reductionsOnPlaces,
+                       @NonNull Collection<? extends Reduction<Transition>> reductionsOnTransitions) {
         if (reduced) {
             throw new IllegalStateException("Petri Net already reduced");
         }
 
+        this.reductionsOnPlaces = reductionsOnPlaces;
+        this.reductionsOnTransitions = reductionsOnTransitions;
 
+        boolean prevReduced = true;
+
+        while (prevReduced) {
+            boolean reducedSmth = false;
+
+            for (Place place : petriNet.getPlaces()) {
+                if (!visitedPlace.contains(place)) {
+                    reducedSmth = reducedSmth || placeDFS(place);
+                }
+            }
+
+            for (Transition transition : petriNet.getTransitions()) {
+                if (!visitedTransition.contains(transition)) {
+                    reducedSmth = reducedSmth || transitionDFS(transition);
+                }
+            }
+
+            visitedTransition.clear();
+            visitedPlace.clear();
+
+            prevReduced = reducedSmth;
+        }
+//тьмок тьмок
+
+//тьмок
         reduced = true;
+    }
+
+    private boolean placeDFS(Place current) {
+        visitedPlace.add(current);
+
+        boolean reducedSmth = false;
+        for (Reduction<Place> reduction : reductionsOnPlaces) {
+            reducedSmth = reducedSmth || reduction.reduceFrom(current);
+        }
+
+        if (reducedSmth)
+            reduceCount++;
+
+        return reducedSmth || current.getOutputs().stream()
+                .map(Edge::getToEndpoint)
+                .filter(transition -> !visitedTransition.contains(transition))
+                .anyMatch(this::transitionDFS);
+    }
+
+    private boolean transitionDFS(Transition current) {
+        visitedTransition.add(current);
+
+        boolean reducedSmth = false;
+        for (Reduction<Transition> reduction : reductionsOnTransitions) {
+            reducedSmth = reducedSmth || reduction.reduceFrom(current);
+        }
+
+        if (reducedSmth)
+            reduceCount++;
+
+        return reducedSmth || current.getOutputs().stream()
+                .map(Edge::getToEndpoint)
+                .filter(place -> !visitedPlace.contains(place))
+                .anyMatch(this::placeDFS);
     }
 }
---
+++ petri-nets-library/src/test/java/hse/se/aaizmaylov/petrinetslibrary/petrinets/basic/analysis/ReducerTest.java
@@ -0,0 +1,74 @@
+package hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.analysis;
+
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.*;
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.reductions.FusionOfSelfLoopPlaces;
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.reductions.FusionOfSelfLoopTransitions;
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.reductions.FusionOfSeriesPlaces;
+import hse.se.aaizmaylov.petrinetslibrary.petrinets.basic.reductions.FusionOfSeriesTransitions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReducerTest {
+
+    // 17d from Murata
+    PetriNet createPetriNetWithoutMarks(List<? super Place> places, List<? super Transition> transitions) {
+        Place p1 = Place.withMarks(0);
+        Place p2 = Place.withMarks(0);
+        Place p3 = Place.withMarks(1);
+        Place p4 = Place.withMarks(0);
+
+        Transition t1 = new TransitionImpl();
+        Transition t2 = new TransitionImpl();
+        Transition t3 = new TransitionImpl();
+        Transition t4 = new TransitionImpl();
+
+        t1.addInput(new FromPlaceToTransitionEdge(p2, t1));
+        t1.addOutput(new FromTransitionToPlaceEdge(t1, p1));
+
+        t2.addInput(new FromPlaceToTransitionEdge(p1, t2));
+        t2.addOutput(new FromTransitionToPlaceEdge(t2, p2));
+
+        t3.addInput(new FromPlaceToTransitionEdge(p2, t3));
+        t3.addInput(new FromPlaceToTransitionEdge(p3, t3));
+        t3.addOutput(new FromTransitionToPlaceEdge(t3, p4));
+
+        t4.addInput(new FromPlaceToTransitionEdge(p4, t4));
+        t4.addOutput(new FromTransitionToPlaceEdge(t4, p3));
+
+        places.add(p1);
+        places.add(p2);
+        places.add(p3);
+        places.add(p4);
+
+        transitions.add(t1);
+        transitions.add(t2);
+        transitions.add(t3);
+        transitions.add(t4);
+
+        return new PetriNet(Arrays.asList(p1, p2, p3, p4), Arrays.asList(t1, t2, t3, t4));
+    }
+
+    @Test
+    void simpleReduction() {
+        List<Place> places = new ArrayList<>();
+        List<Transition> transitions = new ArrayList<>();
+
+        PetriNet petriNet = createPetriNetWithoutMarks(places, transitions);
+
+        Reducer reducer = new Reducer(petriNet);
+
+        reducer.reduce(Arrays.asList(new FusionOfSeriesPlaces(), new FusionOfSelfLoopTransitions()),
+                Arrays.asList(new FusionOfSeriesTransitions(), new FusionOfSelfLoopPlaces()));
+
+        assertEquals(1, places.get(1).getOutputs().size());
+        assertTrue(places.get(1).getInputs().isEmpty());
+
+        assertEquals(1, transitions.get(2).getInputs().size());
+        assertTrue(transitions.get(2).getOutputs().isEmpty());
+    }
+}
