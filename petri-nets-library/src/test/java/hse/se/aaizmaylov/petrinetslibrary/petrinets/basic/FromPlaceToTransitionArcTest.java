8138ed4e098d031409c48cad5fe23131978d4fdc
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-05-04 15:53:21 +0300

    some tests for clone

--- petri-nets-library/src/test/java/hse/se/aaizmaylov/petrinetslibrary/petrinets/basic/FromPlaceToTransitionArcTest.java
+++ petri-nets-library/src/test/java/hse/se/aaizmaylov/petrinetslibrary/petrinets/basic/FromPlaceToTransitionArcTest.java
@@ -23,4 +23,14 @@ class FromPlaceToTransitionArcTest {
 
         assertEquals(Long.valueOf(1), arc.weight());
     }
+
+    @Test
+    void cloneTest() {
+        FromPlaceToTransitionArc arc = new FromPlaceToTransitionArc(new PlaceImpl(0, "p"),
+                new TransitionImpl("t"), 10).clone();
+
+        assertEquals("p", arc.getFromEndpoint().label());
+        assertEquals("t", arc.getToEndpoint().label());
+        assertEquals(Long.valueOf(10), arc.weight());
+    }
 }
