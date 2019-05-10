4f7c1de4ecd4999148dd9e8487965c9937c08c26
Izmaylov Alexandr <zinoviy23@gmail.com>
2019-05-11 00:20:49 +0300

    Coloring vertices with issues in pnml file

--- petri-nets-library/src/main/java/hse/se/aaizmaylov/petrinetslibrary/utils/XmlUtils.java
+++ petri-nets-library/src/main/java/hse/se/aaizmaylov/petrinetslibrary/utils/XmlUtils.java
@@ -1,5 +1,10 @@
 package hse.se.aaizmaylov.petrinetslibrary.utils;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.Attributes;
 
 public class XmlUtils {
@@ -14,5 +19,24 @@ public class XmlUtils {
             sb.deleteCharAt(sb.length() - 1);
 
         return sb.toString();
+    }
+
+    @Contract("null, _ -> null")
+    @Nullable
+    public static Element getElementByTag(@Nullable NodeList list, @NotNull String tag) {
+        if (list == null)
+            return null;
+
+        for (int i = 0, length = list.getLength(); i < length; i++) {
+            if (!(list.item(i) instanceof Element))
+                continue;
+
+            Element el = (Element) list.item(i);
+            if (el.getTagName().equals(tag)) {
+                return el;
+            }
+        }
+
+        return null;
     }
 }
